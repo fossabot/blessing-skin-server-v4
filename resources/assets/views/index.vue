44a0356554f8c7be024cb33398d68a017135afd6
Pig Fang <g-plane@hotmail.com>
2018-01-14 14:21:34 +0800

    style(lint): apply some rules

--- resources/assets/views/index.vue
+++ resources/assets/views/index.vue
@@ -3,22 +3,22 @@
         <Layout>
             <Header>
                 <Menu mode="horizontal" theme="dark" active-name="1">
-                    <div class="layout-logo"></div>
+                    <div class="layout-logo" />
                     <div class="layout-nav">
                         <MenuItem name="1">
-                            <Icon type="ios-navigate"></Icon>
+                            <Icon type="ios-navigate" />
                             Item 1
                         </MenuItem>
                         <MenuItem name="2">
-                            <Icon type="ios-keypad"></Icon>
+                            <Icon type="ios-keypad" />
                             Item 2
                         </MenuItem>
                         <MenuItem name="3">
-                            <Icon type="ios-analytics"></Icon>
+                            <Icon type="ios-analytics"/>
                             Item 3
                         </MenuItem>
                         <MenuItem name="4">
-                            <Icon type="ios-paper"></Icon>
+                            <Icon type="ios-paper"/>
                             Item 4
                         </MenuItem>
                     </div>
@@ -29,7 +29,7 @@
                     <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
                         <Submenu name="1">
                             <template slot="title">
-                                <Icon type="ios-navigate"></Icon>
+                                <Icon type="ios-navigate"/>
                                 Item 1
                             </template>
                             <MenuItem name="1-1">Option 1</MenuItem>
@@ -38,7 +38,7 @@
                         </Submenu>
                         <Submenu name="2">
                             <template slot="title">
-                                <Icon type="ios-keypad"></Icon>
+                                <Icon type="ios-keypad"/>
                                 Item 2
                             </template>
                             <MenuItem name="2-1">Option 1</MenuItem>
@@ -46,7 +46,7 @@
                         </Submenu>
                         <Submenu name="3">
                             <template slot="title">
-                                <Icon type="ios-analytics"></Icon>
+                                <Icon type="ios-analytics"/>
                                 Item 3
                             </template>
                             <MenuItem name="3-1">Option 1</MenuItem>
