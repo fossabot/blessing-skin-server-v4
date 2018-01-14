410a3e8f2c3178673ccff4a010a76634af2dfc29
Pig Fang <g-plane@hotmail.com>
2018-01-14 22:38:50 +0800

    build: better production build

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -15,6 +15,10 @@ module.exports = merge(webpackBaseConfig, {
         chunkFilename: '[name].[hash].chunk.js'
     },
     plugins: [
+        // keep module.id stable when vendor modules does not change
+        new webpack.HashedModuleIdsPlugin(),
+        // enable scope hoisting
+        new webpack.optimize.ModuleConcatenationPlugin(),
         new ExtractTextPlugin({
             filename: '[name].[hash].css',
             allChunks: true
@@ -22,15 +26,15 @@ module.exports = merge(webpackBaseConfig, {
         new webpack.optimize.CommonsChunkPlugin({
             name: 'vendors',
             filename: 'vendors.[hash].js',
-            minChunks (module) {
+            minChunks(module) {
                 return (
-                  module.resource &&
-                  /\.js$/.test(module.resource) &&
-                  module.resource.indexOf(
-                    path.join(__dirname, 'node_modules')
-                  ) === 0
-                )
-              }
+                    module.resource &&
+                    /\.js$/.test(module.resource) &&
+                    module.resource.indexOf(
+                        path.join(__dirname, 'node_modules')
+                    ) === 0
+                );
+            }
         }),
         new webpack.DefinePlugin({
             'process.env': {
@@ -45,7 +49,29 @@ module.exports = merge(webpackBaseConfig, {
         new HtmlWebpackPlugin({
             filename: path.join(__dirname, './resources/views/index.blade.php'),
             template: './resources/assets/template/index.ejs',
-            inject: false
+            inject: true,
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeAttributeQuotes: true
+            },
+            // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+            chunksSortMode: 'dependency'
+        }),
+        // extract webpack runtime and module manifest to its own file in order to
+        // prevent vendor hash from being updated whenever app bundle is updated
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'manifest',
+            minChunks: Infinity
+        }),
+        // This instance extracts shared chunks from code splitted chunks and bundles them
+        // in a separate chunk, similar to the vendor chunk
+        // see: https://webpack.js.org/plugins/commons-chunk-plugin/#extra-async-commons-chunk
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'app',
+            async: 'vendor-async',
+            children: true,
+            minChunks: 3
         })
     ]
 });
