3ff4dc4d589208c1fbf1672fc040664abe488f26
Pig Fang <g-plane@hotmail.com>
2018-01-17 15:20:11 +0800

    feat(ui): add Minecraft font

--- resources/assets/app.vue
+++ resources/assets/app.vue
@@ -3,3 +3,10 @@
         <router-view />
     </div>
 </template>
+
+<style lang="less">
+@font-face {
+    font-family: Minecraft;
+    src: url('./fonts/minecraft.woff2')  format('woff2');
+}
+</style>
