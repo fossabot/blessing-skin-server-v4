2edfdca2f660e809672a900d56c798d8bf200eb2
Pig Fang <g-plane@hotmail.com>
2018-01-18 16:03:44 +0800

    refactor: import axios

--- resources/assets/libs/ajax.js
+++ resources/assets/libs/ajax.js
@@ -1,15 +1,13 @@
 import Vue from 'vue';
 import axios from 'axios';
 
-const util = Object.create(null);
-
 const ajaxUrl = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1/' : '/';
 
-util.ajax = axios.create({
+const ajax = axios.create({
     baseURL: ajaxUrl,
     timeout: 30000
 });
 
-Vue.prototype.$ajax = util.ajax;
+Vue.prototype.$ajax = ajax;
 
-export default util;
+export default ajax;
--- resources/assets/main.js
+++ resources/assets/main.js
@@ -6,6 +6,7 @@ import router from './libs/router';
 import store from './libs/store';
 import i18n from './libs/i18n';
 import apolloProvider from './libs/apollo';
+import './libs/ajax';
 import App from './app.vue';
 
 Vue.use(iView);
