58a2824633e8e30d8c01ba0d3d7f88718c184ede
Pig Fang <g-plane@hotmail.com>
2018-01-18 12:33:15 +0800

    fix(auth): fix refreshing token

--- app/Http/Middleware/RefreshToken.php
+++ app/Http/Middleware/RefreshToken.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Auth;
 use Closure;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Illuminate\Validation\UnauthorizedException;
@@ -33,11 +34,21 @@ class RefreshToken extends BaseMiddleware
         } catch (TokenExpiredException $e) {
             try {
                 $token = $this->auth->refresh();
+                Auth::onceUsingId(
+                    $this
+                        ->auth
+                        ->manager()
+                        ->getPayloadFactory()
+                        ->buildClaimsCollection()
+                        ->toPlainArray()['sub']
+                );
             } catch (JWTException $e) {
                 throw new UnauthorizedException('Token has expired. Please re-login.');
             }
         }
 
-        return $this->setAuthenticationHeader($next($request), $token);
+        $response = $next($request);
+        $response->headers->set('Access-Control-Expose-Headers', 'Authorization');
+        return $this->setAuthenticationHeader($response, $token);
     }
 }
