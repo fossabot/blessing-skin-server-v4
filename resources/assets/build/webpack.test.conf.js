64ed22495f78667f428b50a5408ce59068affc1c
Pig Fang <g-plane@hotmail.com>
2018-02-07 21:18:14 +0800

    build: skip eslint check before unit test

--- resources/assets/build/webpack.test.conf.js
+++ resources/assets/build/webpack.test.conf.js
@@ -3,6 +3,10 @@ const webpackBaseConfig = require('./webpack.base.conf');
 const nodeExternals = require('webpack-node-externals');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+webpackBaseConfig.module.rules = webpackBaseConfig.module.rules.filter(
+    rule => rule.loader !== 'eslint-loader'
+);
+
 module.exports = merge(webpackBaseConfig, {
     devtool: 'inline-cheap-module-source-map',
     externals: [nodeExternals()],
