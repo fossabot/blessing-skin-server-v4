bcc7e4d0af693037c0b6238fb6392b1154b7069e
Pig Fang <g-plane@hotmail.com>
2018-02-11 12:01:57 +0800

    test(ui): fix possible errors

--- resources/assets/tests/views/user/dashboard.test.ts
+++ resources/assets/tests/views/user/dashboard.test.ts
@@ -400,6 +400,7 @@ describe('test `Dashboard` component', () => {
                     .format('YYYY-MM-DD kk:mm:ss')
             }
         });
+        wrapper.update();
         await wrapper.vm.$nextTick();
         expect(wrapper.find('.btn-sign').attributes()!.title).to.equal(
             wrapper.vm.$t('last-sign', {
